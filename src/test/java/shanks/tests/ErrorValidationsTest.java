89e6cc98550b0c6aabce143abe8a2ac377189745
Shankar <sivasankartanjavur@gmail.com>
2024-11-27 16:46:22 +0530

    My Commit

---
+++ src/test/java/shanks/tests/ErrorValidationsTest.java
@@ -0,0 +1,47 @@
+package shanks.tests;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import shanks.TestComponents.BaseTest;
+import shanks.TestComponents.Retry;
+import shanks.pageobject.CartPage;
+import shanks.pageobject.ProductCatalogue;
+
+public class ErrorValidationsTest extends BaseTest {
+
+	@Test(groups= {"ErrorHandling"},retryAnalyzer=Retry.class)
+	public void LoginErrorValidation() throws IOException, InterruptedException {
+
+	
+		landingPage.loginApplication("shank@gmail.com", "Shanks@73");
+		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+
+	}
+	
+
+	@Test
+	public void ProductErrorValidation() throws IOException, InterruptedException
+	{
+
+		String productName = "ZARA COAT 3";
+		ProductCatalogue productCatalogue = landingPage.loginApplication("shankar@gmail.com", "Shanks@73");
+		List<WebElement> products = productCatalogue.getProductList();
+		productCatalogue.addProductToCart(productName);
+		CartPage cartPage = productCatalogue.goToCartPage();
+		Boolean match = cartPage.VerifyProductDisplay("ZARA COAT 33");
+		Assert.assertFalse(match);
+		
+	
+
+	}
+
+	
+	
+
+}
+
